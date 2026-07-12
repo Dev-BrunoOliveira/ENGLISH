e57d986720a72eadc5ac34a6001bd1efd06ca920
Bruno Oliveira <brunooliver2015@outlook.com>
2026-07-12 20:18:24 -0300

    redirecionamento do deploy

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -31,6 +31,9 @@ export function useAuth() {
   const loginWithGoogle = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
+      options: {
+        redirectTo: window.location.origin,
+      },
     });
     if (error) return { success: false, error: error.message };
     return { success: true };
